4e636a2a074390a96c465e29d2788d7d4d1c9760
Camilo Alfonso <cabp393@gmail.com>
2022-08-17 22:22:10 -0400

    only cats

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+export default function App() {
+  const [cats, setCats] = useState([])
+  const [page, setPage] = useState(1)
+  const cant = 5
+  const API_URL = `https://api.thecatapi.com/v1/images/search?limit=${cant}&page=${page}&order=Desc`
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setCats(prev => prev.concat(data)))
+    // console.log(cats)
+  }, [page])
+
+  return (
+    <InfiniteScroll
+      dataLength={cats.length}
+      hasMore={true}
+      next={() => {
+        setPage(prev => prev + 1)
+      }}
+    >
+      <div className="appCont">
+        {cats?.map(cat => {
+          if (cat.width > 500) {
+            return (
+              <div key={`${cat.id}${page}`} className="container">
+                <img src={cat.url} loading="lazy" />
+              </div>
+            )
+          }
+        })}
+      </div>
+    </InfiniteScroll>
+  )
+}
